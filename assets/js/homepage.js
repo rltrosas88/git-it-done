b9250b78da6165733958ac2a7b49ef5236b80b0b
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-04-29 09:13:23 -0500

    1.5 Capture Data Returned from the API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,14 @@
 //a variable called getIserRepos that holds a function
 var getUserRepos = function() {
     //console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+       //console.log("inside", response); 
+       response.json().then(function(data) {
+           console.log(data);
+        });
+    //console.log("outside");
+    });
+
 };
 
 getUserRepos();
