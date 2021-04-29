42f4ca46f48dd495341c1004c4f5e5a7b7d445d6
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-04-29 11:16:04 -0500

    1.6 Update Request Logic with Dynamic Variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,22 @@
 //a variable called getIserRepos that holds a function
-var getUserRepos = function() {
-    //console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-       //console.log("inside", response); 
-       response.json().then(function(data) {
-           console.log(data);
+var getUserRepos = function(user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
         });
-    //console.log("outside");
     });
+    //console.log("function was called");
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+    //    //console.log("inside", response); 
+    //    response.json().then(function(data) {
+    //        console.log(data);
+    //     });
+    //console.log("outside");
+    // });
 
 };
 
