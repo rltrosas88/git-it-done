9866b97afe126a68e3df6efe6ff081f4eabf1971
Ryan Lynn Rosas <rltrosas88@gmail.com>
2021-05-04 19:36:12 -0500

    4.5 handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,12 +3,21 @@ var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoName = function() {
+    //grap repo name from url query string
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
     //console.log(repoName);
-    getRepoIssues(repName);
-    repoNameEl.textContent = repoName;
-}
+
+    if (repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     //console.log(repo);
@@ -31,7 +40,9 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage 
+            document.location.replace("./index.html");
+            //alert("There was a problem with your request!");
         }
     });
 };
